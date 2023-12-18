80002e1657698c99bf824ee4c148e83a81ac3232
Bruno Menezes <brunodmenezes@gmail.com>
2023-12-18 15:25:46 +1300

    Feature: Process ERC-721 information (#19)

--- src/handlers/InputAdded.ts
+++ src/handlers/InputAdded.ts
@@ -2,15 +2,33 @@ import { BlockData, DataHandlerContext, Log } from '@subsquid/evm-processor';
 import { Store } from '@subsquid/typeorm-store';
 import { dataSlice, getNumber, getUint } from 'ethers';
 import { Contract as ERC20 } from '../abi/ERC20';
+import { Contract as ERC721 } from '../abi/ERC721';
 import { events } from '../abi/InputBox';
-import { ERC20PortalAddress, InputBoxAddress } from '../config';
-import { Application, Erc20Deposit, Input, Token } from '../model';
+import {
+    ERC20PortalAddress,
+    ERC721PortalAddress,
+    InputBoxAddress,
+} from '../config';
+import {
+    Application,
+    Erc20Deposit,
+    Erc721Deposit,
+    Input,
+    NFT,
+    Token,
+} from '../model';
 import Handler from './Handler';
+
+const logErrorAndReturnNull =
+    (ctx: DataHandlerContext<Store>) => (reason: any) => {
+        ctx.log.error(reason);
+        return null;
+    };
 
 export default class InputAdded implements Handler {
     constructor(
-        private tokenStorage: Map<String, Token>,
-        private depositStorage: Map<String, Erc20Deposit>,
+        private tokenStorage: Map<String, Token | NFT>,
+        private depositStorage: Map<String, Erc20Deposit | Erc721Deposit>,
         private applicationStorage: Map<String, Application>,
         private inputStorage: Map<String, Input>,
     ) {}
@@ -26,7 +44,7 @@ export default class InputAdded implements Handler {
             const from = dataSlice(input.payload, 21, 41).toLowerCase(); // 20 bytes for address
             const amount = getUint(dataSlice(input.payload, 41, 73)); // 32 bytes for uint256
 
-            let token = this.tokenStorage.get(tokenAddress);
+            let token = this.tokenStorage.get(tokenAddress) as Token;
             if (!token) {
                 const contract = new ERC20(ctx, block.header, tokenAddress);
                 const name = await contract.name();
@@ -44,7 +62,49 @@ export default class InputAdded implements Handler {
             });
             return deposit;
         }
+
         return undefined;
+    }
+
+    async prepareErc721Deposit(
+        input: Input,
+        block: BlockData,
+        ctx: DataHandlerContext<Store>,
+        opts: {
+            inputId: String;
+        },
+    ) {
+        if (input.msgSender !== ERC721PortalAddress) return undefined;
+
+        const tokenAddress = dataSlice(input.payload, 0, 20).toLowerCase(); // 20 bytes for address
+        const from = dataSlice(input.payload, 20, 40).toLowerCase(); // 20 bytes for address
+        const tokenIndex = getUint(dataSlice(input.payload, 40, 72)); // 32 bytes for uint256
+
+        let nft = this.tokenStorage.get(tokenAddress) as NFT;
+        if (!nft) {
+            const contract = new ERC721(ctx, block.header, tokenAddress);
+            const name = await contract
+                .name()
+                .catch(logErrorAndReturnNull(ctx));
+            const symbol = await contract
+                .symbol()
+                .catch(logErrorAndReturnNull(ctx));
+            nft = new NFT({ id: tokenAddress, name, symbol });
+            this.tokenStorage.set(tokenAddress, nft);
+            ctx.log.info(`${tokenAddress} (NFT) stored`);
+        }
+
+        const deposit = new Erc721Deposit({
+            id: input.id,
+            from,
+            token: nft,
+            tokenIndex,
+        });
+
+        this.depositStorage.set(opts.inputId, deposit);
+        ctx.log.info(`${opts.inputId} (Erc721Deposit) stored`);
+
+        return deposit;
     }
 
     async handle(log: Log, block: BlockData, ctx: DataHandlerContext<Store>) {
@@ -82,12 +142,23 @@ export default class InputAdded implements Handler {
                 blockHash: log.block.hash,
                 transactionHash: log.transaction?.hash,
             });
+
             const erc20Deposit = await this.handlePayload(input, block, ctx);
             if (erc20Deposit) {
                 this.depositStorage.set(inputId, erc20Deposit);
                 ctx.log.info(`${inputId} (Erc20Deposit) stored`);
                 input.erc20Deposit = erc20Deposit;
             }
+
+            input.erc721Deposit = await this.prepareErc721Deposit(
+                input,
+                block,
+                ctx,
+                {
+                    inputId,
+                },
+            );
+
             this.inputStorage.set(inputId, input);
             ctx.log.info(`${inputId} (Input) stored`);
         }
--- tests/handlers/InputAdded.test.ts
+++ tests/handlers/InputAdded.test.ts
@@ -1,9 +1,18 @@
 import { dataSlice, getUint } from 'ethers';
-import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { afterEach } from 'node:test';
+import { MockedObject, beforeEach, describe, expect, test, vi } from 'vitest';
 import { Contract } from '../../src/abi/ERC20';
+import { Contract as ERC721 } from '../../src/abi/ERC721';
 import InputAdded from '../../src/handlers/InputAdded';
-import { Application, Erc20Deposit, Token } from '../../src/model';
-import { block, ctx, input, logs } from '../stubs/params';
+import {
+    Application,
+    Erc20Deposit,
+    Erc721Deposit,
+    Input,
+    NFT,
+    Token,
+} from '../../src/model';
+import { block, ctx, input, logErc721Transfer, logs } from '../stubs/params';
 
 vi.mock('../../src/abi/ERC20', async (importOriginal) => {
     const actualMods = await importOriginal;
@@ -16,22 +25,39 @@ vi.mock('../../src/abi/ERC20', async (importOriginal) => {
         Contract,
     };
 });
-vi.mock('../../src/model/', async (importOriginal) => {
-    const actualMods = await importOriginal;
+
+vi.mock('../../src/abi/ERC721', async (importOriginal) => {
+    const Contract = vi.fn();
+
+    Contract.prototype.name = vi.fn();
+    Contract.prototype.symbol = vi.fn();
+
+    return {
+        Contract,
+    };
+});
+
+vi.mock('../../src/model/', async () => {
     const Token = vi.fn();
     const Erc20Deposit = vi.fn();
     const Application = vi.fn();
     const Input = vi.fn();
+    const Erc721Deposit = vi.fn();
+    const NFT = vi.fn();
     return {
-        ...actualMods!,
         Application,
         Token,
         Erc20Deposit,
+        Erc721Deposit,
         Input,
+        NFT,
     };
 });
 
 const ApplicationMock = vi.mocked(Application);
+const InputMock = vi.mocked(Input);
+const NFTStub = vi.mocked(NFT);
+const ERC721DepositStub = vi.mocked(Erc721Deposit);
 
 const tokenAddress = dataSlice(input.payload, 1, 21).toLowerCase(); // 20 bytes for address
 const from = dataSlice(input.payload, 21, 41).toLowerCase(); // 20 bytes for address
@@ -40,10 +66,12 @@ const amount = getUint(dataSlice(input.payload, 41, 73)); // 32 bytes for uint25
 describe('InputAdded', () => {
     let inputAdded: InputAdded;
     let erc20;
+    let erc721: MockedObject<ERC721>;
     const mockTokenStorage = new Map();
     const mockDepositStorage = new Map();
     const mockInputStorage = new Map();
     const mockApplicationStorage = new Map();
+
     beforeEach(() => {
         inputAdded = new InputAdded(
             mockTokenStorage,
@@ -52,6 +80,7 @@ describe('InputAdded', () => {
             mockInputStorage,
         );
         erc20 = new Contract(ctx, block.header, tokenAddress);
+        erc721 = vi.mocked(new ERC721(ctx, block.header, tokenAddress));
         mockTokenStorage.clear();
         mockDepositStorage.clear();
         mockApplicationStorage.clear();
@@ -110,23 +139,27 @@ describe('InputAdded', () => {
             expect(handlePayload).toBe(undefined);
         });
     });
+
     describe('handle', async () => {
         test('call with the correct params', async () => {
             vi.spyOn(inputAdded, 'handle');
             inputAdded.handle(logs[0], block, ctx);
             expect(inputAdded.handle).toBeCalledWith(logs[0], block, ctx);
         });
+
         test('wrong contract address', async () => {
             await inputAdded.handle(logs[1], block, ctx);
             expect(mockInputStorage.size).toBe(0);
             expect(mockApplicationStorage.size).toBe(0);
             expect(mockDepositStorage.size).toBe(0);
         });
+
         test('correct contract address', async () => {
             await inputAdded.handle(logs[0], block, ctx);
             expect(mockApplicationStorage.size).toBe(1);
             expect(mockInputStorage.size).toBe(1);
         });
+
         test('Erc20Deposit Stored', async () => {
             const name = 'SimpleERC20';
             const symbol = 'SIM20';
@@ -172,6 +205,102 @@ describe('InputAdded', () => {
             expect(expectedParams).toHaveBeenCalledWith({
                 id: '0x0be010fa7e70d74fa8b6729fe1ae268787298f54',
                 timestamp,
+            });
+        });
+
+        describe('ERC-721 deposits', () => {
+            const name = 'BrotherNFT';
+            const symbol = 'BRUH';
+
+            beforeEach(() => {
+                erc721.name.mockResolvedValue(name);
+                erc721.symbol.mockResolvedValue(symbol);
+
+                // Returning simple object as the Class type for assertion
+                InputMock.mockImplementationOnce((args) => {
+                    return { ...args } as Input;
+                });
+
+                NFTStub.mockImplementationOnce((args) => {
+                    return { ...args } as NFT;
+                });
+
+                ERC721DepositStub.mockImplementationOnce((args) => {
+                    return { ...args } as Erc721Deposit;
+                });
+            });
+
+            afterEach(() => {
+                vi.clearAllMocks();
+            });
+
+            test('should store the token information', async () => {
+                await inputAdded.handle(logErc721Transfer, block, ctx);
+
+                expect(mockTokenStorage.size).toBe(1);
+                const token = mockTokenStorage.values().next().value;
+                expect(token.name).toEqual(name);
+                expect(token.symbol).toEqual(symbol);
+            });
+
+            test('should store the deposit information', async () => {
+                await inputAdded.handle(logErc721Transfer, block, ctx);
+
+                expect(mockDepositStorage.size).toBe(1);
+                const deposit = mockDepositStorage.values().next().value;
+                expect(deposit.id).toEqual(
+                    '0x0be010fa7e70d74fa8b6729fe1ae268787298f54-1',
+                );
+                expect(deposit.from).toEqual(
+                    logErc721Transfer.transaction.from,
+                );
+                expect(deposit.tokenIndex).toEqual(1n);
+                expect(deposit.token).toEqual({
+                    id: '0x7a3cc9c0408887a030a0354330c36a9cd681aa7e',
+                    name,
+                    symbol,
+                });
+            });
+
+            test('should assign the erc721 deposit information correctly into the input', async () => {
+                await inputAdded.handle(logErc721Transfer, block, ctx);
+
+                expect(mockInputStorage.size).toBe(1);
+                const input = mockInputStorage.values().next().value;
+                expect(input.erc721Deposit).toEqual({
+                    from: '0xa074683b5be015f053b5dceb064c41fc9d11b6e5',
+                    id: '0x0be010fa7e70d74fa8b6729fe1ae268787298f54-1',
+                    token: {
+                        id: '0x7a3cc9c0408887a030a0354330c36a9cd681aa7e',
+                        name,
+                        symbol,
+                    },
+                    tokenIndex: 1n,
+                });
+            });
+
+            test('should handle the absence of name and symbol methods in the ERC-721 contract', async () => {
+                erc721.name.mockRejectedValue(
+                    new Error('No name method implemented on contract'),
+                );
+                erc721.symbol.mockRejectedValue(
+                    new Error('No symbol method implemented on contract'),
+                );
+
+                await inputAdded.handle(logErc721Transfer, block, ctx);
+
+                expect(mockInputStorage.size).toBe(1);
+                const input = mockInputStorage.values().next().value;
+                expect(input.erc721Deposit).toEqual({
+                    from: '0xa074683b5be015f053b5dceb064c41fc9d11b6e5',
+                    id: '0x0be010fa7e70d74fa8b6729fe1ae268787298f54-1',
+                    token: {
+                        id: '0x7a3cc9c0408887a030a0354330c36a9cd681aa7e',
+                        name: null,
+                        symbol: null,
+                    },
+                    tokenIndex: 1n,
+                });
             });
         });
     });
