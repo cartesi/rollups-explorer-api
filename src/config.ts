4de1702ea61620d2b5ebebca105285e691db082a
Bruno Menezes <brunodmenezes@gmail.com>
2024-05-24 08:34:21 +1200

    feat: Add configuration for optimism-mainnet and optimism-sepolia. (#33)

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,15 @@
 import CartesiDAppFactoryMainnet from '@cartesi/rollups/deployments/mainnet/CartesiDAppFactory.json';
 import InputBoxMainnet from '@cartesi/rollups/deployments/mainnet/InputBox.json';
+import CartesiDAppFactoryOptimism from '@cartesi/rollups/deployments/optimism/CartesiDAppFactory.json';
+import InputBoxOptimism from '@cartesi/rollups/deployments/optimism/InputBox.json';
+import CartesiDAppFactoryOptimismSepolia from '@cartesi/rollups/deployments/optimism_sepolia/CartesiDAppFactory.json';
+import InputBoxOptimismSepolia from '@cartesi/rollups/deployments/optimism_sepolia/InputBox.json';
 import CartesiDAppFactorySepolia from '@cartesi/rollups/deployments/sepolia/CartesiDAppFactory.json';
 import InputBoxSepolia from '@cartesi/rollups/deployments/sepolia/InputBox.json';
 import mainnet from '@cartesi/rollups/export/abi/mainnet.json';
 import { lookupArchive } from '@subsquid/archive-registry';
 import { DataSource } from '@subsquid/evm-processor';
+import { optimism, optimismSepolia } from 'viem/chains';
 
 // addresses are the same on all chains
 export const CartesiDAppFactoryAddress =
@@ -59,6 +64,40 @@ export const getConfig = (chainId: number): ProcessorConfig => {
                 from: Math.min(
                     CartesiDAppFactorySepolia.receipt.blockNumber,
                     InputBoxSepolia.receipt.blockNumber,
+                ),
+                finalityConfirmation: parseIntOr({
+                    defaultVal: FINALITY_CONFIRMATION,
+                    value: process.env[BLOCK_CONFIRMATIONS],
+                }),
+            };
+        case 10: //Optimism-Mainnet
+            return {
+                dataSource: {
+                    archive: lookupArchive('optimism-mainnet'),
+                    chain:
+                        process.env[RPC_URL] ??
+                        optimism.rpcUrls.default.http[0],
+                },
+                from: Math.min(
+                    CartesiDAppFactoryOptimism.receipt.blockNumber,
+                    InputBoxOptimism.receipt.blockNumber,
+                ),
+                finalityConfirmation: parseIntOr({
+                    defaultVal: FINALITY_CONFIRMATION,
+                    value: process.env[BLOCK_CONFIRMATIONS],
+                }),
+            };
+        case 11155420: //Optimism-Sepolia
+            return {
+                dataSource: {
+                    archive: lookupArchive('optimism-sepolia'),
+                    chain:
+                        process.env[RPC_URL] ??
+                        optimismSepolia.rpcUrls.default.http[0],
+                },
+                from: Math.min(
+                    CartesiDAppFactoryOptimismSepolia.receipt.blockNumber,
+                    InputBoxOptimismSepolia.receipt.blockNumber,
                 ),
                 finalityConfirmation: parseIntOr({
                     defaultVal: FINALITY_CONFIRMATION,
