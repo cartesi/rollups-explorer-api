8c0a0803d164eb169948ba8a5f38320b51ef9c8a
Bruno Menezes <brunodmenezes@gmail.com>
2026-06-25 11:01:09 +0100

    feat: Add function to get gateway configuration including api-key information.

--- tests/gateways.test.ts
+++ tests/gateways.test.ts
@@ -1,5 +1,6 @@
-import { describe, expect, it } from 'vitest';
-import { archiveNodes } from '../src/gateways';
+import { afterEach } from 'node:test';
+import { describe, expect, it, vi } from 'vitest';
+import { archiveNodes, getArchiveGateway } from '../src/gateways';
 
 describe('Gateways (Squid Archive Nodes)', () => {
     it('should have a list of all supported chains by name', () => {
@@ -43,5 +44,61 @@ describe('Gateways (Squid Archive Nodes)', () => {
         expect(archiveNodes.arbitrumSepolia).toStrictEqual(
             'https://v2.archive.subsquid.io/network/arbitrum-sepolia',
         );
+    });
+
+    describe('getArchiveGateway', () => {
+        afterEach(() => {
+            vi.unstubAllEnvs();
+        });
+
+        it('should throw an error if the ARCHIVE_GATEWAY_API_KEY environment variable is not set', () => {
+            const originalEnv = process.env.ARCHIVE_GATEWAY_API_KEY;
+            delete process.env.ARCHIVE_GATEWAY_API_KEY;
+
+            try {
+                getArchiveGateway('mainnet');
+                throw new Error('Expected getArchiveGateway to throw an error');
+            } catch (error) {
+                expect((error as Error).message).toEqual(
+                    'Required ARCHIVE_GATEWAY_API_KEY environment variable is not set.',
+                );
+            } finally {
+                process.env.ARCHIVE_GATEWAY_API_KEY = originalEnv;
+            }
+        });
+
+        it('should return the correct gateway settings for a supported network', () => {
+            const apiKey = 'test-api-key';
+            vi.stubEnv('ARCHIVE_GATEWAY_API_KEY', apiKey);
+
+            Object.entries(archiveNodes).forEach(([network, url]) => {
+                const gatewaySettings = getArchiveGateway(
+                    network as keyof typeof archiveNodes,
+                );
+                expect(gatewaySettings).toEqual({
+                    url,
+                    apiKey,
+                });
+            });
+        });
+
+        describe('Runtime errors', () => {
+            it('should throw an error if the archive requested is not supported', () => {
+                const apiKey = 'test-api-key';
+                vi.stubEnv('ARCHIVE_GATEWAY_API_KEY', apiKey);
+
+                try {
+                    // @ts-expect-error: Testing unsupported network
+                    getArchiveGateway('lemonade');
+                    throw new Error(
+                        'Expected getArchiveGateway to throw an error',
+                    );
+                } catch (error) {
+                    expect((error as Error).message).toEqual(
+                        `Unsupported gateway: lemonade.\nSupported archive gateways: ${Object.keys(archiveNodes).join(', ')}`,
+                    );
+                }
+            });
+        });
     });
 });
